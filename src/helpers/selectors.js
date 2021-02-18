96274c5076cad72409e121a9494f73ec413ce8db
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-18 14:51:14 -0700

    add helper funct

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,13 +2,31 @@ export function getAppointmentsForDay(state, day) {
   const found = state.days.find((dayObject) => dayObject.name === day);
   if (found === undefined) {
     let result = [];
-    console.log("undefined result", state.days.name);
+    // console.log("undefined result", state.days.name);
     return result;
   }
   const foundMap = found.appointments.map((id) => state.appointments[id]);
-  const timeMap = found.appointments.map((id) => state.appointments[id].time);
+  // const timeMap = found.appointments.map((id) => state.appointments[id].time);
 
   let result = foundMap;
 
   return result;
+}
+
+export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  }
+
+  let interviewer = interview.interviewer;
+  interviewer = interviewer.toString();
+
+  for (const key in state.interviewers) {
+    if (key === interviewer) {
+      return {
+        interviewer: state.interviewers[key],
+        student: interview.student,
+      };
+    }
+  }
 }
